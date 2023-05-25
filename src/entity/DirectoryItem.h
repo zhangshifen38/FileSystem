f38fb6f0e3bba2c5252b8e8bf077e78d94e87f2e
AlexHoring <zhangshifen38@gmail.com>
2023-05-25 14:08:47 +0800

    确定了基本结构，准备逐步完善

---
+++ src/entity/DirectoryItem.h
@@ -0,0 +1,19 @@
+//
+// Created by AlexHoring on 2023/5/25.
+//
+
+#ifndef FILESYSTEM_DIRECTORYITEM_H
+#define FILESYSTEM_DIRECTORYITEM_H
+
+#include <cstdint>
+
+/*
+ * @brief 目录项
+ */
+class DirectoryItem {
+public:
+
+};
+
+
+#endif //FILESYSTEM_DIRECTORYITEM_H
